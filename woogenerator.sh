2a794deac0356f66fbecc5800e76d2c6195b5eaa
derwentx <derwentx@gmail.com>
2016-11-23 10:28:52 +1100

    trying out woogenerator.sh

---
+++ woogenerator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "########"
+echo "Welcome to the WooGenerator interactive script"
+echo " 1. Products"
+echo " 2. Customers"
+echo "Type the number for the function you wish to perform or ctrl-c to exit"
+echo "########"
+read -p "What would you like to sync?" sync
+
+echo "selected $sync"
