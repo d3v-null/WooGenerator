1168c9dc50e7b1057396cc632341e8db57c4065c
derwentx <derwentx@gmail.com>
2016-11-23 20:58:48 +1100

    full colour questions and answers

--- woogenerator.sh
+++ woogenerator.sh
@@ -1,24 +1,130 @@
 #!/bin/bash
 
+# bugs:
+# - doesn't ask for current-special
+# - doesn't print instructions when complete
+# - file info should be more verbose
+# - doesn't do images
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function style_prompt() {
+  printf "${PURPLE}"
+  printf -- "$@"
+  printf "${NC}"
+}
+
+function style_info() {
+  printf "${YELLOW}"
+  printf "$@"
+  printf "${NC}"
+  printf "\n"
+}
+
+function style_warning() {
+  printf "${RED}"
+  printf "$@"
+  printf "${NC}"
+  printf "\n"
+}
+
+function style_success() {
+  printf "${GREEN}"
+  printf "$@"
+  printf "${NC}"
+  printf "\n"
+}
+
+ANSWER_TRUE=1
+ANSWER_FALSE=0
+
+function yes_like() {
+  case "$1" in
+    'y' | 'Y' )
+      # echo "answer is answer_true"
+      return $ANSWER_TRUE
+      ;;
+    'n' | 'N' )
+      # echo "answer is answer_false"
+      return $ANSWER_FALSE
+      ;;
+    *)
+      if [[ "$2" == "$ANSWER_FALSE" ]] ; then
+        # echo "default is answer_false"
+        return $ANSWER_FALSE
+      elif [[ "$2" == "$ANSWER_TRUE" ]] ; then
+        # echo "default is answer_true"
+        return $ANSWER_TRUE
+      fi
+  esac
+}
+
+function binary_prompt() {
+  read -d '' -t 1 -n 10000 discard
+  question_suffix=' '
+  default_answer=$ANSWER_TRUE
+  if [[ ! -z "$2" ]] ; then
+    yes_like "$2" "$default_answer"
+    default_answer=$?
+  fi
+  # echo "default_answer is $default_answer"
+  if [[ "$default_answer" == "$ANSWER_TRUE" ]] ; then
+    question_suffix='(Y/n) '
+  else
+    question_suffix='(y/N) '
+  fi
+  style_prompt "$1 $question_suffix"
+  read -n 1 answer
+  printf '\n'
+  if [[ ! -z "$answer" ]] ; then
+    yes_like "$answer" "$default_answer"
+    response=$?
+    # echo "yes_like $answer $default_answer gives $response"
+  else
+    response=$default_answer
+    # echo "response set to default_answer: $default_answer"
+  fi
+  # echo "response is $response"
+  return $response
+  # if [[ "$response" == "$ANSWER_FALSE" ]] ; then
+  #   echo "response is answer_false"
+  #   return $ANSWER_FALSE
+  # elif [[ "$response" == "$ANSWER_TRUE" ]] ; then
+  #   echo "response is answer_true"
+  #   return $ANSWER_TRUE
+  # fi
+}
+
+
 sync_subject=''
+safe_defaults=false
+download_report_quit=false
+sync_report=false
 sync_command=()
 
 if [[ -z "$@" ]] ; then
   # echo "no args"
-  echo "########"
-  echo "Welcome to the WooGenerator interactive script"
-  echo "Type the number for the function you wish to perform. press ctrl-c to exit at any point"
-  echo " 1. Products (interactive)"
-  echo " 2. Products (safe defaults)"
-  echo " 3. Products (download, report, quit)"
-  echo " 4. Customers (interactive)"
-  echo " 5. Customer (safe defaults)"
-  echo "########"
-  read -p "What would you like to sync? (default: 1) " sync
+  style_info "########"
+  style_info "Welcome to the WooGenerator interactive script"
+  style_info "Type the number for the function you wish to perform. press ctrl-c to exit at any point"
+  style_info " 1. Products (interactive)"
+  style_info " 2. Products (safe defaults)"
+  style_info " 3. Products (download, report, quit)"
+  style_info " 4. Customers (interactive)"
+  style_info " 5. Customer (safe defaults)"
+  style_info "########"
+  style_prompt "What would you like to sync? (default: 1) "
+  read -n 1 sync
+  printf "\n"
 else
   # echo "args $@"
   sync="$1"
 fi
+
 
 case "$sync" in
   1 | '')
@@ -43,95 +149,97 @@ case "$sync" in
     safe_defaults=true
     ;;
   *)
-    echo "invalid selection, try again"
+    style_warning "invalid selection, try again"
     exit
     ;;
 esac
 
+if $safe_defaults ; then echo "safe_defaults" ; fi
+if $download_report_quit ; then echo "download_report_quit" ; fi
+
+
 case "$sync_subject" in
   'products' )
-    echo "You have selected to sync products"
+    style_info "You have selected to sync products"
     sync_command+=("python" "source/generator.py")
 
     if ! $safe_defaults ; then
-      read -p "has the google drive product spreadsheet been updated recently? (Y/n) " download_master
+      binary_prompt "has the google drive product spreadsheet been updated recently?" "y"
+      download_master_answer="$?"
     fi
 
-    case "$download_master" in
-      "n" | "N" )
-        sync_command+=('--skip-download-master')
-        ;;
-      *)
-        sync_command+=('--download-master')
-    esac
-
-    if ! $safe_defaults ; then
-      read -p "would you like to generate a sync report? (slow) (Y/n) " sync_report
+    if [[ "$download_master_answer" == "$ANSWER_TRUE" ]] ; then
+      sync_command+=('--download-master')
+    else
+      sync_command+=('--skip-download-master')
     fi
-    case "$sync_report" in
-      "n" | "N" )
-        sync_command+=('--skip-download-slave' '--skip-sync')
-        sync_command+=('--skip-report')
-        ;;
-      *)
-        sync_command+=('--download-slave' '--do-sync')
-        sync_command+=('--show-report')
-        ;;
-    esac
 
     if ! $safe_defaults ; then
-      read -p "do you want to process specials? (y/N) " specials
+      binary_prompt "would you like to generate a sync report? (slow)" "y"
+      sync_report_answer="$?"
+    fi
+    if [[ "$sync_report_answer" == "$ANSWER_TRUE" ]] ; then
+      sync_command+=('--download-slave' '--do-sync')
+      sync_command+=('--show-report')
+      sync_report=true
+    else
+      sync_command+=('--skip-download-slave' '--skip-sync')
+      sync_command+=('--skip-report')
+    fi
+
+    if ! $safe_defaults ; then
+      binary_prompt "do you want to process specials?" "n"
+      specials_answer="$?"
     elif $download_report_quit ; then
-      specials='y'
+      specials_answer="$ANSWER_TRUE"
     fi
 
-    case "$specials" in
-      "y" | "Y" )
-        sync_command+=('--do-specials')
-        if ! $download_report_quit ; then
-          read -p "what is the current special? (e.g. SP2016-11-20-) " current_special
-          if [ -z $current_special ]
-          then
-            sync_command+=('--add-special-categories')
-            sync_command+=('--current-special' "$current_special")
-          fi
+    if [[ "$sync_report_answer" == "$ANSWER_TRUE" ]] ; then
+      sync_command+=('--do-specials')
+      if ! $download_report_quit ; then
+        style_prompt "what is the current special? (e.g. SP2016-11-20-) "
+        read current_special
+        if [ -z $current_special ]
+        then
+          sync_command+=('--add-special-categories')
+          sync_command+=('--current-special' "$current_special")
         fi
-        ;;
-      *)
+      fi
+    else
         sync_command+=('--skip-specials')
-    esac
-
-    if ! $safe_defaults ; then
-      read -p "do you want to process categories? (y/N) " categories
-    elif $download_report_quit ; then
-      categories='y'
     fi
 
-    case "$categories" in
-      "y" | "Y" )
-        sync_command+=('--do-categories')
-        ;;
-      *)
-        sync_command+=('--skip-categories')
-    esac
-
     if ! $safe_defaults ; then
-      read -p "do you want to process variations? (y/N) " variations
+      binary_prompt "do you want to process categories?" "n"
+      categories_answer="$?"
     elif $download_report_quit ; then
-      variations='y'
+      categories_answer=$ANSWER_TRUE
     fi
 
-    case "$variations" in
-      "y" | "Y" )
-        sync_command+=('--do-variations')
-        ;;
-      *)
-        sync_command+=('--skip-variations')
-    esac
+    if [[ "$categories_answer" == "$ANSWER_TRUE" ]] ; then
+      sync_command+=('--do-categories')
+    else
+      sync_command+=('--skip-categories')
+    fi
+
+    if ! $safe_defaults ; then
+      binary_prompt "do you want to process variations?" "n"
+      variations_answer="$?"
+    elif $download_report_quit ; then
+      variations_answer=$ANSWER_TRUE
+    fi
+
+    if [[ "$variations_answer" == "$ANSWER_TRUE" ]] ; then
+      sync_command+=('--do-variations')
+    else
+      sync_command+=('--skip-variations')
+    fi
 
     # auto update not finished yet
     sync_command+=('--skip-update-slave')
-    # read -p "do you want to automatically update WooCommerce? (unsafe) (y/N) " update
+    # binary_prompt "do you want to automatically update WooCommerce? (unsafe)" "n"
+    # update_answer="$?"
+    # read -n 1 -p
     #
     # case "$update" in
     #   "y" | "Y" )
@@ -141,7 +249,9 @@ case "$sync_subject" in
     #     sync_command+=('--skip-update-slave')
     # esac
     #
-    # read -p "do you want to perform the updates in the report flagged as problematic? (y/N) " update_problematic
+    # binary_prompt "do you want to perform the updates in the report flagged as problematic?" "n"
+    # update_problematic_answer="$?"
+    # read -n 1 -p
     #
     # case "$update_problematic" in
     #   "y" | "Y" )
@@ -152,12 +262,12 @@ case "$sync_subject" in
     # esac
     ;;
   'customers' )
-    echo "You have selected to sync customers"
+    style_info "You have selected to sync customers"
     sync_command+=("python" "source/merger.py")
     ;;
 esac
 
-echo "running" ${sync_command[@]}
+style_info "running" ${sync_command[@]}
 
 try_again=true
 errors_present=false
@@ -171,48 +281,45 @@ while $try_again ; do
     exec "${sync_command[@]}" | tee >(cat - >&5)
   )
   # result=$("${sync_command[@]}" | tee >(cat - >&5))
-  exit_code=$?
+  exit_code="$?"
 
   # printf "\n\n\n\n"
-  echo "exit_code is $exit_code"
+  style_info "exit_code is $exit_code"
   # echo "result is $result"
 
   case "$exit_code" in
-    0 )
-      echo "Completed"
+    "0" )
+      style_success "Completed"
       break
       ;;
-    74 )
-      echo "It looks there were problems accessing files at some point"
-      echo "please check the missing files exist and are readable"
-      read -p "were you able to fix the problems? (y/N) " try_again_answer
-      case "$try_again_answer" in
-        'y' | 'Y' )
-          continue
-          ;;
-      esac
+    "74" )
+      style_warning "It looks there were problems accessing files at some point"
+      style_warning "please check the missing files exist and are readable"
+      binary_prompt "were you able to fix the problems?" "y"
+      try_again_answer="$?"
+      if [[ "$try_again_answer" == "$ANSWER_TRUE" ]] ; then
+        continue
+      fi
       ;;
-    65 )
-      echo "It looks like there are some problems in the data"
-      echo "please fix these problems and try again"
-      read -p "were you able to fix the problems? (y/N) " try_again_answer
-      case "$try_again_answer" in
-        'y' | 'Y' )
-          continue
-          ;;
-      esac
+    "65" )
+      style_warning "It looks like there are some problems in the data"
+      style_warning "please fix these problems and try again"
+      binary_prompt "were you able to fix the problems?" "y"
+      try_again_answer="$?"
+      if [[ "$try_again_answer" == "$ANSWER_TRUE" ]] ; then
+        continue
+      fi
       ;;
-    69 ) # connection error
-      echo "It looks like the connection timed out."
-      read -p "try again with larger timeout? (y/N) " try_again_answer
-      case "$try_again_answer" in
-        'y' | 'Y' )
-          continue
-          ;;
-      esac
+    "69" ) # connection error
+      style_warning "It looks like the connection timed out."
+      binary_prompt "try again with larger timeout?" "y"
+      try_again_answer="$?"
+      if [[ "$try_again_answer" == "$ANSWER_TRUE" ]] ; then
+        continue
+      fi
       ;;
     *)
-      echo "It looks like something went wrong."
+      style_warning "It looks like something went wrong."
       ;;
   esac
   try_again=false
@@ -223,11 +330,13 @@ if ! $errors_present ; then
   checklist=()
   case "$sync_subject" in
     'products' )
-      echo "please check the report files and the generated csv files."
-      checklist=(\
-        "syncing program did not display any errors"\
-        "only products you expect to be created are being created"\
+      style_warning "please complete the following checklist."
+      checklist=(
+        "syncing program did not display any errors"
       )
+      if $sync_report ; then
+        checklist+=("only products you expect to be created are being created")
+      fi
       ;;
     'customers' )
       echo ""
@@ -235,49 +344,49 @@ if ! $errors_present ; then
   esac
 
   for check in "${checklist[@]}"; do
-    read -p "-> $check (Y/n) " no_anomalies
-    case "$no_anomalies" in
-      'n' | 'N' )
-        errors_present=true
-        break
-        ;;
-    esac
+    binary_prompt "-> $check" "y"
+    no_anomalies_answer="$?"
+    if [[ "$no_anomalies_answer" == "$ANSWER_FALSE" ]] ; then
+      errors_present=true
+      break
+    fi
   done
 fi
 
 if ! $errors_present ; then
   case "$sync_subject" in
     'products' )
-      echo "once everything looks ok you can log in to the website and upload the csv files"
-      echo "see https://docs.woocommerce.com/document/product-csv-import-suite/ if you're having trouble"
-      echo "WooCommerce -> csv import suite -> merge products"
-      echo "WooCommerce -> csv import suite -> merge variations"
+      style_info "once everything looks ok you can log in to the website and upload the csv files"
+      style_info "see https://docs.woocommerce.com/document/product-csv-import-suite/ if you're having trouble"
+      style_info "WooCommerce -> csv import suite -> merge products"
+      style_info "WooCommerce -> csv import suite -> merge variations"
       ;;
     'customers' )
-      echo ''
+      style_info ''
       ;;
   esac
 
-  read -p "did the sync complete as expected? (Y/n) " no_anomalies
-  case "$no_anomalies" in
-    'n' | 'N' )
-      errors_present=true
-      ;;
-  esac
+  binary_prompt "did the sync complete as expected?" "y"
+  no_anomalies_answer="$?"
+
+  if [[ "$no_anomalies_answer" == "$ANSWER_FALSE" ]] ; then
+    errors_present=true
+  fi
 fi
 
 if $errors_present ; then
-  read -p "would you like to email the developer a report of the errors? (Y/n) " email_dev
-  case "$email_dev" in
-    'n' | 'N' )
-      echo "all done!"
-      exit
-      ;;
-    *)
-      read -p "any additional information to report?" additional_info
+  binary_prompt "would you like to email the developer a report of the errors?" "y"
+  email_dev_answer="$?"
 
-      echo "creating report..."
-  esac
+  read  -n 1 -p "would you like to email the developer a report of the errors? (Y/n) " email_dev
+  if [[ "$email_dev_answer" == "$ANSWER_FALSE" ]] ; then
+    style_success "all done!"
+    exit
+  else
+    style_prompt "any additional information to report? "
+    read additional_info
+    style_info "creating report..."
+  fi
 else
-  echo "looks like everything worked"
+  style_success "looks like everything worked"
 fi
