38f9b6cb5d1cfdff661299cf1007e00a680c5410
derwentx <derwentx@gmail.com>
2017-07-06 23:15:37 +1000

    tests pre moving product_parser_args into settings namespace

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 from unittest import TestCase, main, skip, TestSuite, TextTestRunner
+import argparse
 
 from context import woogenerator
 from woogenerator.coldata import ColDataWoo
@@ -21,14 +22,15 @@ class TestGenerator(TestCase):
         self.settings.local_work_dir = tests_datadir
         self.settings.local_live_config = None
         self.settings.local_test_config = "generator_config_test.yaml"
+        self.settings.download_master = False
         self.settings.master_file = os.path.join(tests_datadir, "generator_sample.csv")
         # self.config_path = os.path.join(tests_datadir, "generator_config_test.yaml")
         self.override_args = ""
-        # self.override_args = "--config-file=" + self.config_path
+        # self.override_args = "--skip-download-master"
 
     def test_init_settings(self):
-        # Registrar.DEBUG_MESSAGE = True
-        # Registrar.DEBUG_ERROR = True
+        Registrar.DEBUG_MESSAGE = True
+        Registrar.DEBUG_ERROR = True
         self.settings = init_settings(
             settings=self.settings,
             override_args=self.override_args,
@@ -37,6 +39,26 @@ class TestGenerator(TestCase):
         self.assertEqual(self.settings.master_name, "GDrive")
         self.assertEqual(self.settings.slave_name, "WooCommerce")
         self.assertEqual(self.settings.merge_mode, "sync")
+        self.assertEqual(self.settings.schema, "WC")
+        self.assertEqual(self.settings.download_master, False)
+
+
+    def test_populate_master_parsers(self):
+        self.test_init_settings()
+        self.parsers = argparse.Namespace()
+        # self.settings.product_parser_args = {
+        #     'import_name': self.settings.import_name,
+        #     'item_depth': self.settings.item_depth,
+        #     'taxo_depth': self.settings.taxo_depth,
+        # }
+
+        self.parsers = populate_master_parsers(
+            self.parsers,
+            self.settings
+        )
+
+        print(getattr(self.parsers, 'ma').tabulate())
+
 
 if __name__ == '__main__':
     main()
