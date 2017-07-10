aa73ab336aaf60173c3d2ff48ed1b3ee7fdcd466
derwentx <derwentx@gmail.com>
2017-07-10 14:23:12 +1000

    added merger tests

--- tests/test_merger.py
+++ tests/test_merger.py
@@ -10,26 +10,25 @@ from woogenerator.parsing.woo import ImportWooProduct, CsvParseWoo, CsvParseTT, 
 from woogenerator.utils import TimeUtils, Registrar, SanitationUtils
 from woogenerator.utils import (HtmlReporter, ProgressCounter, Registrar,
                                 SanitationUtils, TimeUtils, DebugUtils)
-from woogenerator.config import (ArgumentParserProd, ArgumentParserProtoProd,
-                                 SettingsNamespaceProd, init_settings, ParsersNamespace)
-from woogenerator.generator import populate_master_parsers
+from woogenerator.config import (ArgumentParserUser, SettingsNamespaceUser, 
+                                 init_settings, ParsersNamespace)
+from woogenerator.merger import populate_master_parsers
 
 from context import tests_datadir
 
 class TestGenerator(TestCase):
     def setUp(self):
 
-        self.settings = SettingsNamespaceProd()
+        self.settings = SettingsNamespaceUser()
         self.settings.local_work_dir = tests_datadir
         self.settings.local_live_config = None
-        self.settings.local_test_config = "generator_config_test.yaml"
-        self.settings.master_dialect_suggestion = "SublimeCsvTable"
+        self.settings.local_test_config = "merger_config_test.yaml"
+        self.settings.master_dialect_suggestion = "ActOut"
         self.settings.download_master = False
-        self.settings.master_file = os.path.join(tests_datadir, "generator_sample.csv")
-        # self.settings.download_limit = 10
+        self.settings.master_file = os.path.join(tests_datadir, "100_users.csv")
+        # self.settings.master_parse_limit = 10
         self.override_args = ""
         self.parsers = ParsersNamespace()
-
 
         Registrar.DEBUG_MESSAGE = True
         Registrar.DEBUG_ERROR = True
@@ -40,14 +39,16 @@ class TestGenerator(TestCase):
         self.settings = init_settings(
             settings=self.settings,
             override_args=self.override_args,
-            argparser_class=ArgumentParserProd
+            argparser_class=ArgumentParserUser
         )
-        self.assertEqual(self.settings.master_name, "GDrive")
-        self.assertEqual(self.settings.slave_name, "WooCommerce")
-        self.assertEqual(self.settings.merge_mode, "sync")
-        self.assertEqual(self.settings.schema, "CA")
+        self.assertEqual(self.settings.master_name, "ACT")
+        self.assertEqual(self.settings.slave_name, "WORDPRESS")
         self.assertEqual(self.settings.download_master, False)
-        self.assertEqual(self.settings.master_client_args["dialect_suggestion"], "SublimeCsvTable")
+        self.assertEqual(
+            self.settings.master_client_args["limit"],
+            self.settings.master_parse_limit
+        )
+        self.assertEqual(self.settings.master_client_args["dialect_suggestion"], "ActOut")
 
     def test_populate_master_parsers(self):
         self.test_init_settings()
@@ -65,24 +66,11 @@ class TestGenerator(TestCase):
         #number of objects:
         self.assertEqual(
             len(self.parsers.master.objects.values()),
-            163
-        )
-        self.assertEqual(
-            len(self.parsers.master.items.values()),
-            144
-        )
-        self.assertEqual(
-            len(self.parsers.master.products.values()),
-            48
+            99
         )
 
-        prod_list = WooProdList(self.parsers.master.products.values())
-        self.assertEqual(
-            prod_list[0].codesum,
-            "ACARA-CAL"
-        )
-        # print(SanitationUtils.coerce_bytes(prod_list.tabulate(tablefmt='simple')))
-
+        obj_list = self.parsers.master.get_obj_list()
+        print(SanitationUtils.coerce_bytes(obj_list.tabulate(tablefmt='simple')))
 
 if __name__ == '__main__':
     main()
