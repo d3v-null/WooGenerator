98716689c35def6f4a3118d2f2a9e9f86f63f66d
derwentx <derwentx@gmail.com>
2017-07-11 14:38:35 +1000

    generator dummy data

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,16 +1,17 @@
 from __future__ import print_function
 import os
 # import sys
-from unittest import TestCase, main, skip, TestSuite, TextTestRunner
+from pprint import pformat
+import unittest
+from unittest import TestCase
+from tabulate import tabulate
 import argparse
 
 from context import woogenerator
-from woogenerator.coldata import ColDataWoo
-from woogenerator.parsing.woo import ImportWooProduct, CsvParseWoo, CsvParseTT, WooProdList
-from woogenerator.utils import TimeUtils, Registrar, SanitationUtils
-from woogenerator.utils import (HtmlReporter, ProgressCounter, Registrar,
-                                SanitationUtils, TimeUtils, DebugUtils)
-from woogenerator.conf.parser import ArgumentParserProd, ArgumentParserProtoProd
+from woogenerator.parsing.woo import WooProdList
+from woogenerator.parsing.special import SpecialGruopList
+from woogenerator.utils import Registrar, SanitationUtils
+from woogenerator.conf.parser import ArgumentParserProd
 from woogenerator.conf.namespace import SettingsNamespaceProd, init_settings, ParsersNamespace
 from woogenerator.generator import populate_master_parsers
 
@@ -25,16 +26,20 @@ class TestGenerator(TestCase):
         self.settings.local_test_config = "generator_config_test.yaml"
         self.settings.master_dialect_suggestion = "SublimeCsvTable"
         self.settings.download_master = False
-        self.settings.master_file = os.path.join(tests_datadir, "generator_sample.csv")
+        self.settings.master_file = os.path.join(tests_datadir, "generator_master_dummy.csv")
+        self.settings.specials_file = os.path.join(tests_datadir, "generator_specials_dummy.csv")
+        self.settings.do_specials = True
         # self.settings.download_limit = 10
         self.override_args = ""
         self.parsers = ParsersNamespace()
 
 
-        # Registrar.DEBUG_MESSAGE = True
+        Registrar.DEBUG_MESSAGE = True
         # Registrar.DEBUG_ERROR = True
         # Registrar.DEBUG_ABSTRACT = True
         # Registrar.DEBUG_PARSER = True
+        # Registrar.DEBUG_WARN = False
+        # Registrar.DEBUG_PROGRESS = False
 
     def test_init_settings(self):
         self.settings = init_settings(
@@ -48,6 +53,7 @@ class TestGenerator(TestCase):
         self.assertEqual(self.settings.schema, "CA")
         self.assertEqual(self.settings.download_master, False)
         self.assertEqual(self.settings.master_client_args["dialect_suggestion"], "SublimeCsvTable")
+        self.assertEqual(self.settings.spec_gid, None)
 
     def test_populate_master_parsers(self):
         self.test_init_settings()
@@ -71,18 +77,59 @@ class TestGenerator(TestCase):
             len(self.parsers.master.items.values()),
             144
         )
-        self.assertEqual(
-            len(self.parsers.master.products.values()),
-            48
-        )
 
         prod_list = WooProdList(self.parsers.master.products.values())
-        self.assertEqual(
-            prod_list[0].codesum,
-            "ACARA-CAL"
-        )
+        self.assertEqual(len(prod_list), 48)
+        first_prod = prod_list[0]
+        self.assertEqual(first_prod.codesum, "ACARA-CAL")
+        self.assertEqual(first_prod.parent.codesum, "ACARA-CA")
+        self.assertEqual(first_prod.specials, ['SP2016-08-12-ACA', 'EOFY2016-ACA'])
+        self.assertEqual(first_prod.product_type, "simple")
+        self.assertEqual(first_prod.depth, 4)
+        self.assertTrue(first_prod.isItem)
+        self.assertTrue(first_prod.isProduct)
+        self.assertFalse(first_prod.isCategory)
+        self.assertFalse(first_prod.isRoot)
+        self.assertFalse(first_prod.isTaxo)
+        self.assertFalse(first_prod.isVariable)
+        self.assertFalse(first_prod.isVariation)
+        for key, value in {
+                'DNR': u'59.97',
+                'DPR': u'57.47',
+                'RNR': u'',
+                'RPR': u'',
+                'WNR': u'99.95',
+                'WPR': u'84.96',
+                'height': u'235',
+                'length': u'85',
+                'price': u'',
+                'weight': u'1.08',
+                'width': u'85'
+        }.items():
+            self.assertEqual(first_prod[key], value)
+        # print("pformat:\n%s" % pformat(dict(first_prod)))
+        # print("dir:")
+        # print(pformat(dir(first_prod)))
+        # print("vars")
+        # print(pformat(vars(first_prod)))
+        # for attr in ["depth"]:
+            # print("first_prod.%s: %s" % (attr, pformat(getattr(first_prod, attr))))
         # print(SanitationUtils.coerce_bytes(prod_list.tabulate(tablefmt='simple')))
 
+        spec_list = SpecialGruopList(self.parsers.special.rule_groups.values())
+        if Registrar.DEBUG_MESSAGE:
+            Registrar.register_message(
+                SanitationUtils.coerce_bytes(spec_list.tabulate(tablefmt='simple'))
+            )
+        first_group = spec_list[0]
+        if Registrar.DEBUG_MESSAGE:
+            Registrar.register_message(
+                "first group:\n%s\npformat@dict:\n%s\npformat@dir:\n%s\n" % (
+                    SanitationUtils.coerce_bytes(tabulate(first_group.children, tablefmt='simple')),
+                    pformat(dict(first_group)),
+                    pformat(dir(first_group))
+                )
+            )
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
