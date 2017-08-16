58d89be8dce1f9cbb3b8c8b8919db1eaa635cdfc
derwentx <derwentx@gmail.com>
2017-08-16 13:41:49 +1000

    💄 explicit utils.__all__

--- woogenerator/utils/__init__.py
+++ woogenerator/utils/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """utils module used by woogenerator."""
 
-import sys
-import os
+# import sys
+# import os
 # MODULE_PATH = os.path.dirname(__file__)
 #
 # sys.path.insert(0, MODULE_PATH)
@@ -14,3 +14,13 @@ from contact import NameUtils, AddressUtils
 from reporter import HtmlReporter
 from clock import TimeUtils
 from inheritence import InheritenceUtils, overrides
+
+__all__ = [
+    SanitationUtils, DescriptorUtils, SeqUtils, DebugUtils,
+    Registrar, ValidationUtils, PHPUtils, ProgressCounter,
+    UnicodeCsvDialectUtils, FileUtils,
+    NameUtils, AddressUtils,
+    HtmlReporter,
+    TimeUtils,
+    InheritenceUtils, overrides
+]
