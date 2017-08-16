98acd1b57e825d4e5dce2cf54fe4c04c8280e593
derwentx <derwentx@gmail.com>
2017-08-16 17:10:18 +1000

    ignore unused import in __init__.py

--- woogenerator/utils/__init__.py
+++ woogenerator/utils/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """utils module used by woogenerator."""
 
+from __future__ import absolute_import
+
 import sys
 import os
 
@@ -9,20 +11,10 @@ MODULE_LOCATION = os.path.dirname(MODULE_PATH)
 PACKAGE_LOCATION = os.path.dirname(MODULE_LOCATION)
 sys.path.insert(0, PACKAGE_LOCATION)
 
-from core import (SanitationUtils, DescriptorUtils, SeqUtils, DebugUtils,
+from .core import (SanitationUtils, DescriptorUtils, SeqUtils, DebugUtils,
                         Registrar, ValidationUtils, PHPUtils, ProgressCounter,
                         UnicodeCsvDialectUtils, FileUtils)
-from contact import NameUtils, AddressUtils
-from reporter import HtmlReporter
-from clock import TimeUtils
-from inheritence import InheritenceUtils, overrides
-
-__all__ = [
-    SanitationUtils, DescriptorUtils, SeqUtils, DebugUtils,
-    Registrar, ValidationUtils, PHPUtils, ProgressCounter,
-    UnicodeCsvDialectUtils, FileUtils,
-    NameUtils, AddressUtils,
-    HtmlReporter,
-    TimeUtils,
-    InheritenceUtils, overrides
-]
+from .contact import NameUtils, AddressUtils
+from .reporter import HtmlReporter
+from .clock import TimeUtils
+from .inheritence import InheritenceUtils, overrides
